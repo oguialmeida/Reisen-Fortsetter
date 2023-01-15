bc131e205caf80bccc07fafe8410eb68a78b32ee
Guilherme Gomes <guilherme.gomes6875@gmail.com>
2023-01-15 20:28:36 -0300

    Adding movements

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import pygame
 
-# Inicia a janela
 pygame.init()
-# Define o tamanho dele
 gameWindow = pygame.display.set_mode([1200, 600])
-# Define um nome para a janela
 pygame.display.set_caption('Reisen Fortsetter')
 
-# Define uma cor de fundo
 def draw():
     gameWindow.fill([119, 136, 153])
 
-
 def loop():
-    gameLoop = True # Enquanto a janela aberta for vedadeira
-    while gameLoop:
-        for event in pygame.event.get(): # Vai cair em uma condição caso algúm evento seja disparado
-            if event.type == pygame.QUIT: # Saber qual evento que foi feito e fazer uma ação relacionada a isso
-                pygame.quit()
-                break
-        draw()
-        pygame.display.update()
+    gameLoop = True
+    x, y, speed = 600, 300, 0.5
+    
+    pygame.time.delay(50)
 
+    while gameLoop:
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+        comands = pygame.key.get_pressed()
+
+        if comands[pygame.K_UP]:
+            y -= speed
+        elif comands[pygame.K_DOWN]:
+            y += speed
+        elif comands[pygame.K_LEFT]:
+            x -= speed
+        elif comands[pygame.K_RIGHT]:
+            x += speed
+
+        draw()
+
+        pygame.draw.circle(gameWindow, (0,255,0), (x, y), 50)
+        pygame.display.update()
 
 if __name__ == '__main__':
     loop()
