1ec44860bc4034230edc744e294962d660aac6f5
Guilherme Gomes <gui.rock08@gmail.com>
2023-06-03 11:19:34 -0300

    improving functions

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
 import pygame
 
-pygame.init()
-gameWindow = pygame.display.set_mode([1200, 600])
-pygame.display.set_caption('Reisen Fortsetter')
+def initialize_game():
+    """Inicializa o Pygame e configura a janela do jogo."""
+    pygame.init()
+    gameWindow = pygame.display.set_mode([1200, 600])
+    pygame.display.set_caption('Reisen Fortsetter')
+    return gameWindow
 
-def draw():
+def draw(gameWindow):
+    """Preenche a janela do jogo com uma cor de fundo."""
     gameWindow.fill([119, 136, 153])
 
-def loop():
+def handle_events():
+    """Lida com os eventos do Pygame, como fechar a janela do jogo."""
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+def handle_movement(comands, x, y, speed):
+    """Lida com o movimento do objeto com base nas teclas pressionadas."""
+    if comands[pygame.K_UP]:
+        y -= speed
+    elif comands[pygame.K_DOWN]:
+        y += speed
+    elif comands[pygame.K_LEFT]:
+        x -= speed
+    elif comands[pygame.K_RIGHT]:
+        x += speed
+    return x, y
+
+def draw_circle(gameWindow, x, y):
+    """Desenha um círculo na janela do jogo nas coordenadas (x, y)."""
+    pygame.draw.circle(gameWindow, (0,255,0), (x, y), 50)
+
+def update_display():
+    """Atualiza a exibição da janela do jogo."""
+    pygame.display.update()
+
+def game_loop(gameWindow):
+    """Loop principal do jogo."""
     gameLoop = True
     x, y, speed = 600, 300, 0.5
     
     pygame.time.delay(50)
 
     while gameLoop:
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            
+        handle_events()
+
         comands = pygame.key.get_pressed()
+        x, y = handle_movement(comands, x, y, speed)
 
-        if comands[pygame.K_UP]:
-            y -= speed
-        elif comands[pygame.K_DOWN]:
-            y += speed
-        elif comands[pygame.K_LEFT]:
-            x -= speed
-        elif comands[pygame.K_RIGHT]:
-            x += speed
-
-        draw()
-
-        pygame.draw.circle(gameWindow, (0,255,0), (x, y), 50)
-        pygame.display.update()
+        draw(gameWindow)
+        draw_circle(gameWindow, x, y)
+        update_display()
 
 if __name__ == '__main__':
-    loop()
+    gameWindow = initialize_game()
+    game_loop(gameWindow)
